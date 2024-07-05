6f81903b3aab8fdba2f00e63d7a144ffdf430d4c
jonathan delvig <delvigjon@hotmail.fr>
2024-07-05 17:12:52 +0200

    exo2 update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,7 +19,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark.destroy
-    redirect_to @bookmark.list, notice: "Bookmark was successfully destroyed."
+    redirect_to list_path(@bookmark.list), notice: "Bookmark was successfully destroyed."
   end
 
   private
