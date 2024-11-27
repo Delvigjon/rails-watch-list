5206eb004ace1c573eff0e50f901c947c7155506
jonathan delvig <delvigjon@hotmail.fr>
2024-11-27 11:34:08 +0100

    nexathlist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
     def home
       @lists = List.all # Remplacez List.all par la logique pour récupérer vos listes
     end
-  end
+    def contact
+      # Logique pour la page de contact (optionnelle)
+    end
+end
   
